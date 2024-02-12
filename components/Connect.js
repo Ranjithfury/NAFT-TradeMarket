7ed4ebac9bda8506502bf8994128ec538c120321
ARJUN RAHUL VIJI <avrahulkanna17@gmail.com>
2024-02-12 17:35:47 +0530

    Add Feature to Connect Wallet inside the DApp

---
+++ components/Connect.js
@@ -0,0 +1,37 @@
+import {useContext, useCallback} from "react";
+import { Web3ModalContext } from '../contexts/Web3ModalProvider';
+import styles from "../components/Navbar.module.css";
+
+
+const Connect = () => {
+
+    function ellipseAddress(
+        address,
+        width) {
+        return `${address?.slice(0, width + 2)}...${address?.slice(-width)}`;
+      }
+    
+      const { account, connect, disconnect } = useContext(Web3ModalContext);
+    
+      console.log(account);
+    
+      console.log(account);
+      const handleConnectWallet = useCallback(() => { // Callback to handle Wallet Connection
+        console.log("called");
+        connect();
+      }, [connect]);
+    
+      const handleDisconnectWallet = useCallback(() => {  // Callback to handle Wallet Disconnection
+        disconnect();
+      }, [disconnect]);
+
+    return (
+     !account ? 
+        (<button className={styles.signupButton} onClick={handleConnectWallet}>Connect Wallet</button>):  // If no account was found, it shows this
+        (<button className={styles.signupButton} onClick={handleDisconnectWallet}>{ellipseAddress(account)}</button>) // Else it shows the Address
+    
+    )
+}
+
+
+export default Connect;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Footer from "../components/footer/Footer"
+import Connect from '../components/Connect';
+import Web3ModalProvider from '../contexts/Web3ModalProvider';
 
 const HomePage = () => {
   return (
     <div>
-
+          <Web3ModalProvider>
+            <Connect />
+            </Web3ModalProvider>
               <h1 style={{ color: 'white' }}>Welcome to Next.js!</h1>
               <p style={{ color: 'white' }}>This is the home page.</p>
               <Footer/>
